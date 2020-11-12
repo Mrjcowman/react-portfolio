c3713de7b07a1fb56066e208ae36c064e1c3ac07
Jacob Cowan <mrjcowman@gmail.com>
2020-11-11 22:04:17 -0800

    Initialize workspace, import assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/"><Index /></Route>
+        <Route exact path="/portfolio"><Portfolio /></Route>
+        <Route exact path="/contact"><Contact /></Route>
+        <Route path="*"><Page404 /></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
