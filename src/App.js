e9bf68e42e8362d13c17be17216ca98d7be0c779
Jacob Cowan <mrjcowman@gmail.com>
2020-11-11 23:21:57 -0800

    Add header to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from 'react';
+
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
@@ -9,9 +12,11 @@ import Contact from './components/Contact';
 import Page404 from './components/Page404';
 
 function App() {
+
   return (
     <BrowserRouter>
       <Navbar />
+      <Header />
       <Switch>
         <Route exact path="/"><About /></Route>
         <Route exact path="/portfolio"><Portfolio /></Route>
