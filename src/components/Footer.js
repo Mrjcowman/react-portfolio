8388eb145b3d3df7706b9de738614c42fd208227
Jacob Cowan <mrjcowman@gmail.com>
2020-11-11 22:37:37 -0800

    Change class to className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer class="footer bg-smokey-bottom border-top border-dark text-light text-center fixed-bottom pb-3">
+        <footer className="footer bg-smokey-bottom border-top border-dark text-light text-center fixed-bottom pb-3">
             <small>Made by Jacob Cowan &copy; 2020</small>
         </footer>
     )
